489fef568b0a9bcf08dd50414b99d44694811f22
Jure Grabnar <jgrabnar@biosistemika.com>
2016-07-18 21:58:24 +0200

    Add default actions

--- ruby_bot/app/controllers/application_controller.rb
+++ ruby_bot/app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
 
   def reset
-    # show no message
     respond_to do |format|
       format.json do
-        render json: {
-          msg: "reset"
-        }
+        render json: {}, status: :ok
       end
     end
   end
@@ -15,19 +12,38 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.json do
         render json: {
-          msg: "select"
+          "hero":"npc_dota_hero_lina","command":"SELECT"
         }
       end
     end
   end
 
   def chat
+    respond_to do |format|
+      format.json do
+        render json: {}, status: :ok
+      end
+    end
   end
 
   def levelup
+    respond_to do |format|
+      format.json do
+        render json: {
+          abilityIndex: -1
+        }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      format.json do
+        render json: {
+          "x": "4000", "y": "4000", "z": "380", "command": "MOVE"
+        }
+      end
+    end
   end
 
 end
